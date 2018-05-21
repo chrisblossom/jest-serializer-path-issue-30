da70427b847488aeb363ec2920593274bf9bcc61
Chris Blossom <cbloss@gmail.com>
2018-05-21 13:39:04 -0700

    link packages

--- packages/test-package-2/lib/test-package-2.test.js
+++ packages/test-package-2/lib/test-package-2.test.js
@@ -1,10 +1,14 @@
 'use strict';
 
+const testPackage1 = require('test-package-1');
 const testPackage2 = require('./test-package-2');
 
 describe('testPackage2', () => {
     test('works', () => {
-        const result = testPackage2();
+        const result = {
+            testPackage1: testPackage1(),
+            testPackage2: testPackage2(),
+        }
 
         expect(result).toMatchSnapshot();
     });
